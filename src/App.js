8c1827f94f35fe93a93f1c81963685a64c9471ed
Kweeka <mtalbi29@yahoo.com>
2023-03-31 12:07:33 +0100

    Add setup files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-lg text-orange-700 font-semibold">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
