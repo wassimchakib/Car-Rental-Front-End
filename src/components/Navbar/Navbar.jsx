c7664b184d43e81d1f5e78cf8a7f7ecda5de54bc
wassimchakib <wassimchakib@hotmail.com>
2023-04-01 21:51:07 +0000

    Add routes

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+  <nav>
+    <h2>RentIt</h2>
+    <ul>
+      <li><Link to="/cars">Cars</Link></li>
+      <li><Link to="/reserve">Reserve</Link></li>
+      <li><Link to="/my-reservations">My Reservations</Link></li>
+      <li><Link to="add">Add A Car</Link></li>
+      <li><Link to="delete">Delete A Car</Link></li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
