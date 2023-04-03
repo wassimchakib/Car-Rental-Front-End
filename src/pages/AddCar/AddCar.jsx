f764865eade0ad85630c945536082676a07b5519
wassimchakib <wassimchakib@hotmail.com>
2023-04-03 15:00:35 +0000

    Add handleSubmit function

--- src/pages/AddCar/AddCar.jsx
+++ src/pages/AddCar/AddCar.jsx
@@ -8,34 +8,56 @@ const AddCar = () => {
   const [year, setYear] = useState('');
   const [price, setPrice] = useState('');
   const [type, setType] = useState('');
+  const [formInfo, setFormInfo] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormInfo(
+      {
+        name,
+        description,
+        color,
+        year,
+        price,
+        type,
+      },
+    );
+    console.log(formInfo);
+    setName('');
+    setDescription('');
+    setColor('');
+    setYear('');
+    setPrice('');
+    setType('');
+  };
 
   return (
-    <>
+    <div className="form__container">
       <h2>Add A Car</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">
           Name:
-          <input type="text" required onChange={setName} id="name" />
+          <input type="text" required onChange={(e) => setName(e.target.value)} id="name" value={name} />
         </label>
         <label htmlFor="description">
           Description:
-          <textarea onChange={setDescription} id="description" />
+          <textarea onChange={(e) => setDescription(e.target.value)} value={description} id="description" />
         </label>
         <label htmlFor="color">
           Color
-          <input type="text" onChange={setColor} id="color" />
+          <input type="text" onChange={(e) => setColor(e.target.value)} value={color} id="color" />
         </label>
         <label htmlFor="year">
           Year
-          <input type="text" required onChange={setYear} id="year" />
+          <input type="text" required onChange={(e) => setYear(e.target.value)} value={year} id="year" />
         </label>
         <label htmlFor="price">
           Price
-          <input type="text" required onChange={setPrice} id="price" />
+          <input type="text" required onChange={(e) => setPrice(e.target.value)} value={price} id="price" />
         </label>
         <label htmlFor="type">
           Type:
-          <select id="type" onChange={setType}>
+          <select id="type" onChange={(e) => setType(e.target.value)} value={type}>
             <option value="Sport">Sport</option>
             <option value="Pickup">Pickup</option>
             <option value="Super">Super</option>
@@ -45,9 +67,9 @@ const AddCar = () => {
             <option value="Micro">Micro</option>
           </select>
         </label>
+        <input type="submit" value="Submit" />
       </form>
-      <p>{name.toString}</p>
-    </>
+    </div>
   );
 };
 
