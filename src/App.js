7235680cbd8254dd5e9f71e3df37e19da9b28132
wassimchakib <wassimchakib@hotmail.com>
2023-04-03 11:29:44 +0000

    Add Car component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import AddCar from './pages/AddCar/AddCar';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
             <Route exact path="/cars" element={<h1>Cars Page</h1>} />
             <Route exact path="/reserve" element={<h1>Reserve Page</h1>} />
             <Route exact path="/my-reservations" element={<h1>My Reservations Page</h1>} />
-            <Route exact path="/add" element={<h1>Add Page</h1>} />
+            <Route exact path="/add" Component={AddCar} />
             <Route exact path="/delete" element={<h1>Delete Page</h1>} />
           </Routes>
         </div>
---
+++ src/pages/AddCar/AddCar.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AddCar = () => <h1>Add Car</h1>;
+
+export default AddCar;
