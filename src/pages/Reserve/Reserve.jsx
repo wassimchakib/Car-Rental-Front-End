77d4d93fd30c02f05d78290ec71ce12a55340c30
Kweeka <mtalbi29@yahoo.com>
2023-04-03 21:42:45 +0100

    Add reserve page

---
+++ src/pages/Reserve/Reserve.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReservationForm from '../../components/ReservationForm/ReservationForm';
+import './reserve.css';
+
+const Reserve = () => (
+  <div className="background flex justify-center pt-[10rem] h-full">
+    <ReservationForm />
+  </div>
+);
+
+export default Reserve;
