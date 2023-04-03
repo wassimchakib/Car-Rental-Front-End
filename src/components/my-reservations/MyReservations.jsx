b47cadb9c5f33de973eab7d10b1cff4efc5e3b24
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2023-04-03 15:25:09 +0500

    Create card containers in my_reservations page

--- src/components/my-reservations/MyReservations.jsx
+++ src/components/my-reservations/MyReservations.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import CarImage from '../../assets/images/Corolla.jpeg';
+import Camry from '../../assets/images/Camry.jpeg';
 
 // StyleSheet
 import './MyReservations.css';
 
 const MyReservations = () => (
-  <div className="grid-container">
-    <h1>Cars in my reservations</h1>
+  <div className="card-container">
+    <div className="reservation-card">
+      <img className="car_img" src={CarImage} alt="" />
+      <h1 className="car-name">Toyota Corolla</h1>
+    </div>
+    <div className="reservation-card">
+      <img className="car_img" src={Camry} alt="" />
+      <h1 className="car-name">Toyota Camry</h1>
+    </div>
+    <div className="reservation-card">
+      <h1 className="car-name">Toyota Corolla</h1>
+    </div>
+    <div className="reservation-card">
+      <h1 className="car-name">Toyota Corolla</h1>
+    </div>
+    <div className="reservation-card">
+      <h1 className="car-name">Toyota Corolla</h1>
+    </div>
 
   </div>
 );
