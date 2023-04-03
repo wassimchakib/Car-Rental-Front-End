7e441be35917d2c891cec0d90354a7bfa139aba5
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2023-04-03 14:13:39 +0500

    Create MyReservations component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import MyReservations from './components/my-reservations/MyReservations';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
           <Routes>
             <Route exact path="/cars" element={<h1>Cars Page</h1>} />
             <Route exact path="/reserve" element={<h1>Reserve Page</h1>} />
-            <Route exact path="/my-reservations" element={<h1>My Reservations Page</h1>} />
+            <Route exact path="/my-reservations" element={<MyReservations />} />
             <Route exact path="/add" element={<h1>Add Page</h1>} />
             <Route exact path="/delete" element={<h1>Delete Page</h1>} />
           </Routes>
---
+++ src/components/my-reservations/MyReservations.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// StyleSheet
+import './MyReservations.css';
+
+const MyReservations = () => (
+  <div className="grid-container">
+    <h1>Cars in my reservations</h1>
+
+  </div>
+);
+
+export default MyReservations;
