091fed0c5688f59f9de49ac8c5971d9f38a33182
wassimchakib <wassimchakib@hotmail.com>
2023-04-03 18:29:18 +0000

    Add styling

--- src/pages/AddCar/AddCar.jsx
+++ src/pages/AddCar/AddCar.jsx
@@ -44,6 +44,12 @@ const AddCar = () => {
     setImages(newImages);
   };
 
+  const deleteImage = (i) => {
+    const newImages = [...images];
+    newImages.splice(i, 1);
+    setImages(newImages);
+  };
+
   const handleImage = (e, index) => {
     const newImages = [...images];
     newImages[index] = e.target.value;
@@ -51,61 +57,63 @@ const AddCar = () => {
   };
 
   return (
-    <div className="form__container">
-      <h2>Add A Car</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">
-          Name:
-          <input type="text" required onChange={(e) => setName(e.target.value)} id="name" value={name} />
-        </label>
-        <label htmlFor="description">
-          Description:
-          <textarea onChange={(e) => setDescription(e.target.value)} value={description} id="description" />
-        </label>
-        <label htmlFor="color">
-          Color
-          <input type="text" onChange={(e) => setColor(e.target.value)} value={color} id="color" />
-        </label>
-        <label htmlFor="year">
-          Year
-          <input type="text" required onChange={(e) => setYear(e.target.value)} value={year} id="year" />
-        </label>
-        <label htmlFor="price">
-          Price
-          <input type="text" required onChange={(e) => setPrice(e.target.value)} value={price} id="price" />
-        </label>
-        <label htmlFor="type">
-          Type:
-          <select id="type" onChange={(e) => setType(e.target.value)} value={type}>
-            <option value="Sport">Sport</option>
-            <option value="Pickup">Pickup</option>
-            <option value="Super">Super</option>
-            <option value="Coupe">Coupe</option>
-            <option value="Limousine">Limousine</option>
-            <option value="Convertible">Convertible</option>
-            <option value="Micro">Micro</option>
-          </select>
-        </label>
-        <br />
-        <h2 id="images">
-          Images:
-          <button id="addimage" type="button" onClick={() => addImage()}>
-            <FaPlus />
-          </button>
-        </h2>
-        {
-          images.map((data, i) => (
-            <div className="image__input" key={`image ${i + 1} `}>
-              <input type="text" onChange={(e) => handleImage(e, i)} value={data} placeholder={`image_url_${i + 1}`} />
-              <button type="button">
-                <FaTrash />
-              </button>
-            </div>
-          ))
-        }
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
+    <section id="add_car">
+      <div className="form__container">
+        <h2>Add A Car</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">
+            Name:
+            <input type="text" required onChange={(e) => setName(e.target.value)} id="name" value={name} />
+          </label>
+          <label htmlFor="description">
+            Description:
+            <textarea onChange={(e) => setDescription(e.target.value)} value={description} id="description" />
+          </label>
+          <label htmlFor="color">
+            Color
+            <input type="text" onChange={(e) => setColor(e.target.value)} value={color} id="color" />
+          </label>
+          <label htmlFor="year">
+            Year
+            <input type="text" required onChange={(e) => setYear(e.target.value)} value={year} id="year" />
+          </label>
+          <label htmlFor="price">
+            Price
+            <input type="text" required onChange={(e) => setPrice(e.target.value)} value={price} id="price" />
+          </label>
+          <label htmlFor="type">
+            Type:
+            <select id="type" onChange={(e) => setType(e.target.value)} value={type}>
+              <option value="Sport">Sport</option>
+              <option value="Pickup">Pickup</option>
+              <option value="Super">Super</option>
+              <option value="Coupe">Coupe</option>
+              <option value="Limousine">Limousine</option>
+              <option value="Convertible">Convertible</option>
+              <option value="Micro">Micro</option>
+            </select>
+          </label>
+          <br />
+          <h2 id="images">
+            Images:
+            <button id="addimage" type="button" onClick={() => addImage()}>
+              <FaPlus />
+            </button>
+          </h2>
+          {
+            images.map((data, i) => (
+              <div className="image__input" key={`image ${i + 1} `}>
+                <input type="text" onChange={(e) => handleImage(e, i)} value={data} placeholder={`image_url_${i + 1}`} />
+                <button type="button" onClick={() => deleteImage(i)}>
+                  <FaTrash />
+                </button>
+              </div>
+            ))
+          }
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </section>
   );
 };
 
