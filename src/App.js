546d4cd85338f2b369ebcfebe239f3924121b252
Kweeka <mtalbi29@yahoo.com>
2023-04-04 15:48:20 +0100

    update classname to avoid conflict with tailwind

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Reserve from './pages/Reserve/Reserve';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
-        <div className="container">
+        <div className="app_container">
           <Routes>
             <Route exact path="/cars" element={<h1>Cars Page</h1>} />
-            <Route exact path="/reserve" element={<h1>Reserve Page</h1>} />
+            <Route exact path="/reserve" element={<Reserve />} />
             <Route exact path="/my-reservations" element={<h1>My Reservations Page</h1>} />
             <Route exact path="/add" element={<h1>Add Page</h1>} />
             <Route exact path="/delete" element={<h1>Delete Page</h1>} />
