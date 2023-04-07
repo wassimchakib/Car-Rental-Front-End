a75d75738aa7a1769d39b6e28b2e9a8f6e4a36ee
wassimchakib <wassimchakib@hotmail.com>
2023-04-07 01:03:01 +0000

    Add Delete Reservation Method

--- src/redux/reservation/reservationSlice.js
+++ src/redux/reservation/reservationSlice.js
@@ -2,6 +2,8 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const SHOW_RESERVATIONS = 'car-rental/reservation/SHOW';
+const ADD_RESERVATION = 'car-rental/reservation/ADD';
+const DELETE_RESERVATION = 'car-rental/reservation/DELETE';
 
 // Method getReservations
 export const getReservations = createAsyncThunk(SHOW_RESERVATIONS, async (thunkAPI) => {
@@ -21,7 +23,7 @@ export const getReservations = createAsyncThunk(SHOW_RESERVATIONS, async (thunkA
 });
 
 // Method AddReservation
-export const addReservation = createAsyncThunk(SHOW_RESERVATIONS, async (reservation, thunkAPI) => {
+export const addReservation = createAsyncThunk(ADD_RESERVATION, async (reservation, thunkAPI) => {
   const API_URL = 'http://localhost:1800/api/v1/reservations';
   const token = localStorage.getItem('token');
   const requestOptions = {
@@ -31,7 +33,24 @@ export const addReservation = createAsyncThunk(SHOW_RESERVATIONS, async (reserva
     },
   };
   try {
-    return await axios.get(API_URL, { reservation }, requestOptions);
+    return await axios.post(API_URL, { reservation }, requestOptions);
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err.response.data.error);
+  }
+});
+
+// Method DeleteReservation
+export const deleteReservation = createAsyncThunk(DELETE_RESERVATION, async (id, thunkAPI) => {
+  const API_URL = 'http://localhost:1800/api/v1/reservations';
+  const token = localStorage.getItem('token');
+  const requestOptions = {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  try {
+    return await axios.delete(API_URL, { id }, requestOptions);
   } catch (err) {
     return thunkAPI.rejectWithValue(err.response.data.error);
   }
@@ -47,6 +66,7 @@ const reservationSlice = createSlice({
     list: '',
   },
   extraReducers: (builder) => {
+    // Get Reservations
     builder.addCase(getReservations.pending, (state) => ({
       ...state,
       isLoading: true,
@@ -66,6 +86,8 @@ const reservationSlice = createSlice({
       error: action.payload,
     }));
 
+    // Add Reservation
+
     builder.addCase(addReservation.pending, (state) => ({
       ...state,
       isLoading: true,
@@ -83,6 +105,27 @@ const reservationSlice = createSlice({
       ...state,
       isLoading: false,
       errors: action.payload.data.errors,
+    }));
+
+    // Delete Reservation
+
+    builder.addCase(deleteReservation.pending, (state) => ({
+      ...state,
+      isLoading: true,
+      error: '',
+    }));
+
+    builder.addCase(deleteReservation.fulfilled, (state, action) => ({
+      ...state,
+      isLoading: false,
+      success: true,
+      id: action.payload.data.data,
+    }));
+
+    builder.addCase(deleteReservation.rejected, (state, action) => ({
+      ...state,
+      isLoading: false,
+      error: action.payload.data.error,
     }));
   },
 });
