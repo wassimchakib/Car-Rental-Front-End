b6008f49c556fae11e5ee9809b9c26cc5bff47fa
Profsain <profsaincrown@gmail.com>
2023-04-09 04:06:37 +0000

    extract CarCard component

--- src/components/cars-components/CarList.js
+++ src/components/cars-components/CarList.js
@@ -1,34 +1,32 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { useState, useEffect } from 'react';
 import ReactPaginate from 'react-paginate';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import CarCard from './CarCard';
 import { car } from './cars';
 import './CarsList.css';
 
-const items = [...Array(100).keys()];
+// const items = [...Array(100).keys()];
 
-const ItemsCard = ({ currentItems }) =>(
-  <>
-    <div className='cardGrid'>
-      {
-        currentItems && currentItems.map((item) => (
-          <div className='cardCol'>
-            <img className='carImg' src="https://imgd-ct.aeplcdn.com/1056x660/cw/ec/36129/MercedesBenz-G63-AMG-New-Exterior-136618.jpg?wm=0&q=75" alt='image'/>
-            <div className='carText'>
-              <h3 className='carTitle'>Car Title #{item}</h3>
-              <p className='carIntro'>this is a short car description. this is a short car description.</p>
-            </div>
-          </div>
-        ))
-      }
-    </div>
-  </>
-  
-);
+// const ItemsCard = ({ currentItems }) =>(
+//   <>
+//     <div className='cardGrid'>
+//       {
+//         currentItems && currentItems.map((item) => (
+//           <div className='cardCol'>
+//             <img className='carImg' src={item.image} alt={item.name}/>
+//             <div className='carText'>
+//               <h3 className='carTitle'>{item.name}</h3>
+//               <p className='carIntro'>{
+//                   item.description.length > 50 ? `${item.description.slice(0, 55)}...` : item.description
+//               }</p>
+//             </div>
+//           </div>
+//         ))
+//       }
+//     </div>
+//   </> 
+// );
 
 const CarList = ({ itemsPerPage }) => {
   const [currentItems, setCurrentItems] = useState(null);
@@ -39,8 +37,8 @@ const CarList = ({ itemsPerPage }) => {
     // fetch data from API later
 
     const endOffset = itemOffset + itemsPerPage;
-    setCurrentItems(items.slice(itemOffset, endOffset));
-    setPageCount(Math.ceil(items.length / itemsPerPage));
+    setCurrentItems(car.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(car.length / itemsPerPage));
   }, [itemOffset, itemsPerPage]);
 
   const handlePageClick = (event) => {
@@ -50,12 +48,11 @@ const CarList = ({ itemsPerPage }) => {
 
   return (
     <>
-      {/* <ItemsCard currentItems={currentItems} /> */}
       <Container>
-        <ItemsCard currentItems={currentItems} />
+        <CarCard currentItems={currentItems} />
       </Container>
 
-      <div className='pagination'>
+      <div className='paginationContainer'>
         <ReactPaginate
           nextLabel="next >"
           onPageChange={handlePageClick}
