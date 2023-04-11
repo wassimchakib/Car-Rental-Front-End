84290e460e17abcd300343aed3ee236ef6deb2e6
Kweeka <mtalbi29@yahoo.com>
2023-04-11 11:12:13 +0100

    Add tests for reservations page

--- src/__tests__/reservations/myReservations.test.js
+++ src/__tests__/reservations/myReservations.test.js
@@ -1,93 +1,111 @@
-// import { Provider } from 'react-redux';
-// import { render, act, screen } from '@testing-library/react';
-// import MyReservations from '../../components/my-reservations/MyReservations';
-// import cars from '../data/cars';
-// import { configureStore } from '@reduxjs/toolkit';
-// import carsSlice from '../../redux/carsSlice';
-// import { getCarsAsync } from '../../redux/cars/carsSlice';
-// import store from '../../redux/store';
+import { Provider } from 'react-redux';
+import { render, act, screen } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
+import axios from 'axios';
+import reservations from '../data/reservations.json';
+import MyReservations from '../../pages/my-reservation/MyReservations';
+import reservationSlice, { getReservations } from '../../redux/reservation/reservationSlice';
 
-// const store = configureStore({
-//   reducer: {
-//     cars: carsSlice,
-//   },
-// });
+jest.mock('axios');
+
+const store = configureStore({
+  reducer: {
+    reservation: reservationSlice,
+  },
+});
 
 describe('My reservations page', () => {
-  beforeEach(() => {
-    // getCarsAsync.mockResolvedValue(cars);
+  beforeEach(async () => {
+    axios.get.mockResolvedValue({ data: reservations });
+    await act(() => store.dispatch(getReservations()));
   });
 
   test('Reservations page should render a list of cars', async () => {
-    // const { container } = await act(() =>
-    //   render(<Provider store={store}><MyReservations /></Provider>
-    // ));
+    const { container } = await act(() => render(
+      <Provider store={store}>
+        <MyReservations />
+      </Provider>,
+    ));
 
-    // const carsEl = await container.querySelectorAll('.reservation-card');
-    // expect(carsEl.length).toBe(cars.length);
+    const carsEl = container.querySelectorAll('.reservation-card');
+    expect(carsEl.length).toBe(reservations.data.reservations.length);
   });
 
   test('Car card component should have an image of car', async () => {
-    // const { container } = await act(() =>
-    //   render(<Provider store={store}><MyReservations /></Provider>
-    // ));
+    const { container } = await act(() => render(
+      <Provider store={store}>
+        <MyReservations />
+      </Provider>,
+    ));
 
-    // const carEl = await container.querySelector('.reservation-card');
-    // expect(carEl.querySelector('img')).not.toBe(null);
-    // expect(carEl.querySelector('img').src).toBe(cars[0].image);
+    const carEl = container.querySelector('.reservation-card');
+    expect(carEl.querySelector('img')).toBeInTheDocument();
+    expect(carEl.querySelector('img').src)
+      .toBe(reservations
+        .data
+        .reservations[0]
+        .car
+        .images[0]
+        .url);
   });
 
   test('Car card component should have the name of car', async () => {
-    // const { container } = await act(() =>
-    //   render(<Provider store={store}><MyReservations /></Provider>
-    // ));
+    await act(() => render(
+      <Provider store={store}>
+        <MyReservations />
+      </Provider>,
+    ));
 
-    // const carName = await screen.findByText(cars[0].name);
-    // expect(carName).not.toBe(null);
+    const carName = await screen.findByText(reservations
+      .data
+      .reservations[0]
+      .car
+      .name);
+
+    expect(carName).toBeInTheDocument();
   });
 
   test('Car card component should have the description of car', async () => {
-    // const { container } = await act(() =>
-    //   render(<Provider store={store}><MyReservations /></Provider>
-    // ));
+    await act(() => render(
+      <Provider store={store}>
+        <MyReservations />
+      </Provider>,
+    ));
 
-    // const carDescription = await screen.findByText(cars[0].description.slice(0, 15));
-    // expect(carDescription).not.toBe(null);
-  });
+    const carDescription = await screen.findByText(reservations
+      .data
+      .reservations[0]
+      .car
+      .description
+      .slice(0, 15));
 
-  test('Car card component should have a pick up date', async () => {
-    // const { container } = await act(() =>
-    //   render(<Provider store={store}><MyReservations /></Provider>
-    // ));
-
-    // const carPickUpDate = await screen.findByText(cars[0].starting_date);
-    // expect(carPickUpDate).not.toBe(null);
-  });
-
-  test('Car card component should have a return date', async () => {
-    // const { container } = await act(() =>
-    //   render(<Provider store={store}><MyReservations /></Provider>
-    // ));
-
-    // const carReturnDate = await screen.findByText(cars[0].ending_date);
-    // expect(carReturnDate).not.toBe(null);
+    expect(carDescription).toBeInTheDocument();
   });
 
   test('Car card component should have a price', async () => {
-    // const { container } = await act(() =>
-    //   render(<Provider store={store}><MyReservations /></Provider>
-    // ));
+    await act(() => render(
+      <Provider store={store}>
+        <MyReservations />
+      </Provider>,
+    ));
 
-    // const carPrice = await screen.findByText(`$${cars[0].price} per day);
-    // expect(carPrice).not.toBe(null);
+    const carPrice = await screen.findByText(`$${reservations
+      .data
+      .reservations[0]
+      .car
+      .price} per day`);
+
+    expect(carPrice).toBeInTheDocument();
   });
 
   test('Car card component should have a cancel reservation button', async () => {
-    // const { container } = await act(() =>
-    //   render(<Provider store={store}><MyReservations /></Provider>
-    // ));
+    await act(() => render(
+      <Provider store={store}>
+        <MyReservations />
+      </Provider>,
+    ));
 
-    // const button = await screen.findByText('Cancel Reservation');
-    // expect(button).not.toBe(null);
+    const button = await screen.findByText('Cancel reservation');
+    expect(button).toBeInTheDocument();
   });
 });
