97deb8c3596f9d69010b539ee7ea3f768e734076
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2023-04-12 15:45:20 +0500

    Create tests for the car details page

--- src/__tests__/car_details/carDetails.test.js
+++ src/__tests__/car_details/carDetails.test.js
@@ -4,7 +4,7 @@ import { render, act, screen } from '@testing-library/react';
 import { configureStore } from '@reduxjs/toolkit';
 import axios from 'axios';
 import carSlice, { getCars } from '../../redux/car/carSlice';
-import CarList from '../../components/cars-components/CarList';
+import CarDetails from '../../components/cars-components/CarDetails';
 import cars from '../data/allCars.json';
 
 jest.mock('axios');
@@ -15,7 +15,7 @@ const store = configureStore({
   },
 });
 
-describe('Cars page', () => {
+describe('CarDetails page', () => {
   beforeEach(async () => {
     axios.get.mockResolvedValue({ data: cars });
     await act(() => store.dispatch(getCars()));
@@ -24,11 +24,11 @@ describe('Cars page', () => {
   it('should have an image of the car', async () => {
     const { container } = await act(() => render(
       <Provider store={store}>
-        <CarList />
+        <CarDetails />
       </Provider>,
     ));
 
-    const carEl = container.querySelector('.cardCol');
+    const carEl = container.querySelector('.carDetailsImage');
     expect(carEl.querySelector('img')).toBeInTheDocument();
     expect(carEl.querySelector('img').src).toBe(
       cars.data.cars[0].images[0].url,
@@ -42,7 +42,7 @@ describe('Cars page', () => {
   it('should have a name', async () => {
     await act(() => render(
       <Provider store={store}>
-        <CarList />
+        <CarDetails />
       </Provider>,
     ));
 
@@ -51,10 +51,22 @@ describe('Cars page', () => {
     expect(carName).toBeInTheDocument();
   });
 
+  it('should have a color', async () => {
+    await act(() => render(
+      <Provider store={store}>
+        <CarDetails />
+      </Provider>,
+    ));
+
+    const carColor = await screen.findByText(cars.data.cars[0].color);
+
+    expect(carColor).toBeInTheDocument();
+  });
+
   it('should have a description', async () => {
     await act(() => render(
       <Provider store={store}>
-        <CarList />
+        <CarDetails />
       </Provider>,
     ));
 
@@ -65,15 +77,27 @@ describe('Cars page', () => {
     expect(carDescription).toBeInTheDocument();
   });
 
-  it('should have a page title', async () => {
+  it('should have a price', async () => {
     await act(() => render(
       <Provider store={store}>
-        <CarList />
+        <CarDetails />
       </Provider>,
     ));
 
-    const title = await screen.findByText('Your Luxury Car for your Comfort');
+    const carPrice = await screen.findByText(cars.data.cars[0].price);
 
-    expect(title).toBeInTheDocument();
+    expect(carPrice).toBeInTheDocument();
+  });
+
+  it('should have a reserve button', async () => {
+    await act(() => render(
+      <Provider store={store}>
+        <CarDetails />
+      </Provider>,
+    ));
+
+    const button = await screen.findByText('Reserve Car');
+
+    expect(button).toBeInTheDocument();
   });
 });
